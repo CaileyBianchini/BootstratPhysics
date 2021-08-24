c6482fad23579f70d7aac177946fa33820fc34fb
Cailey <cailey.m.bianchini@gmail.com>
2021-08-24 16:56:18 -0500

    balls can now bounce

--- ProjectPhysics/PhysicsGame.cpp
+++ ProjectPhysics/PhysicsGame.cpp
@@ -17,14 +17,15 @@ bool PhysicsGame::startup()
 
 	m_scene = new PhysicsScene();
 	m_scene->setTimeStep(0.01f);
-	/*m_scene->setGravity({0.0f, -10.0f});*/
+	m_scene->setGravity({0.0f, -10.0f});
 
 	Sphere* ball = new Sphere(glm::vec2 (20.0f, 0.0f), glm::vec2(), 1, 6, glm::vec4(0.5f, 0.2f, 1.0f, 1.0f));
 	m_scene->addActor(ball);
-	ball->applyForce(glm::vec2(-5.0f, 0.0f));
+	/*ball->applyForce(glm::vec2(-5.0f, -5.0f));*/
 
-	Sphere* orb = new Sphere(glm::vec2(0.0f,0.0f), glm::vec2(), 1, 6, glm::vec4(1.0f, 0.2f, 1.0f, 1.0f));
+	Sphere* orb = new Sphere(glm::vec2(0.0f,0.0f), glm::vec2(), 5, 6, glm::vec4(1.0f, 0.2f, 1.0f, 1.0f));
 	m_scene->addActor(orb);
+	/*orb->applyForce(glm::vec2(0.0f, -5.0f));*/
 
 	Plane* surface = new Plane(glm::vec2(0.0f, 1.0f), -30, glm::vec4(4.0f, 0.5f, 1.0f, 1.0f));
 	m_scene->addActor(surface);

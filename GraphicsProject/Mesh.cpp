faf9a4b34a92861c112f99b1a24d4539eb26dc62
Cailey <cailey.m.bianchini@gmail.com>
2021-09-30 09:52:59 -0500

    added shader class

--- GraphicsProject/Mesh.cpp
+++ GraphicsProject/Mesh.cpp
@@ -16,6 +16,25 @@ Mesh::Mesh()
 	//Bind vertex array
 	glBindVertexArray(m_vertexArrayObject);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBufferObject);
+
+	//define the vertives for a quad
+	Vertex verticies[6];
+	//triangle 0
+	verticies[0].position = { -0.5f, 0.0f, 0.5f, 1.0f };                                 // or glm::vec4()
+	verticies[1].position = { 0.5f, 0.0f, 0.5f, 1.0f };
+	verticies[2].position = { -0.5f, 0.0f, 0.5f, 1.0f };
+	//triangle 1
+	verticies[3].position = { 0.5f, 0.0f, 0.5f, 1.0f };
+	verticies[4].position = { -0.5f, 0.0f, -0.5f, 1.0f };
+	verticies[4].position = { 0.5f, 0.0f, -0.5f, 1.0f };
+
+	//fill vertex buffer
+	glBufferData(
+		GL_ARRAY_BUFFER,     //type of buffer
+		sizeof(Vertex) * 6,           //size in bytes of all vertices
+		verticies,                          //all verticeis
+		GL_STATIC_DRAW      //how the data will update
+	);
 }
 
 Mesh::~Mesh()

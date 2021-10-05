81f30b3005c68019690736492d78052ded3124d1
Cailey <cailey.m.bianchini@gmail.com>
2021-10-05 10:22:22 -0500

    added camera

--- GraphicsProject/Quad.cpp
+++ GraphicsProject/Quad.cpp
@@ -15,13 +15,13 @@ Quad::Vertex* Quad::generateVertices(unsigned int& vertexCount, unsigned int& tr
 	vertices = new Vertex[vertexCount];
 
 	//triangle 0
-	vertices[0].position = { -3.0f, 0.0f, 3.0f, 1.0f };
-	vertices[1].position = { 3.0f, 0.0f, 3.0f, 1.0f };
-	vertices[2].position = { -3.0f, 0.0f, -3.0f, 1.0f };
+	vertices[0].position = { -2.0f, 0.0f, 2.0f, 1.0f };
+	vertices[1].position = { 2.0f, 0.0f, 2.0f, 1.0f };
+	vertices[2].position = { -2.0f, 0.0f, -2.0f, 1.0f };
 	//triangle 1
-	vertices[3].position = { 3.0f, 0.0f, 3.0f, 1.0f };
-	vertices[4].position = { -3.0f, 0.0f, -3.0f, 1.0f };
-	vertices[5].position = { 3.0f, 0.0f, -3.0f, 1.0f };
+	vertices[3].position = { 2.0f, 0.0f, 2.0f, 1.0f };
+	vertices[4].position = { -2.0f, 0.0f, -2.0f, 1.0f };
+	vertices[5].position = { 2.0f, 0.0f, -2.0f, 1.0f };
 
 
 	for (int i = 0; i < vertexCount; i++)
--- GraphicsProject/World.cpp
+++ GraphicsProject/World.cpp
@@ -13,17 +13,17 @@ void World::start()
 	m_quad.start();
 
 	//create camera transforms
-	m_viewMatrix = glm::lookAt(
-		glm::vec3(10.f, 10.0f, 10.0f),
+	m_camra.setTransform(glm::lookAt(
+		glm::vec3(5.f, 5.0f, 5.0f),
 		glm::vec3(0.0f),
 		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
+	));
 
 	m_projectionMatrix = glm::perspective(
-		glm::pi<float>() / 4.0f,
+		m_camra.getFeildOfVeiw() * glm::pi<float>() / 180.0f,
 		(float)m_width / (float)m_height,
-		0.001f,
-		1000.0f
+		m_camra.getNearClip(),
+		m_camra.getFarClip()
 	);
 }
 
@@ -42,5 +42,5 @@ void World::end()
 
 glm::mat4 World::getProjectionViewMode1()
 {
-	return m_projectionMatrix * m_viewMatrix * m_quad.getTransform();
+	return m_projectionMatrix * m_camra.getTransform() * m_quad.getTransform();
 }
